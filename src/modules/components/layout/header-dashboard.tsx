182546f9962bf6bc1e5ae34bee42c235c819c3cb
MAC <ronyortiz@MACs-iMac.local>
2025-10-18 22:15:51 -0400

    IC next proyect setup + dashboard, modules, sidebar

---
+++ src/modules/components/layout/header-dashboard.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { routes } from "@/config/routes";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import DarkMode from "@/components/shared/dark-mode";
+
+const HeaderDashboard = () => {
+  return (
+    <header className="bg-background fixed top-0 right-0 z-50 flex h-16 w-full items-center justify-between border-b px-2 py-2">
+      <div className="container mx-auto flex w-full items-center justify-between gap-1 sm:gap-2">
+        {/* //INFO: MENU & LOGO  & NAV LINKS */}
+        <div className="flex flex-shrink-0 items-center gap-2 p-1">
+          <SidebarTrigger />
+          <Link href={routes.home} className="flex flex-row items-center gap-0">
+            <h6 className="text-primary text-xl font-bold tracking-tight">
+              Multi
+            </h6>
+            <h6 className="text-foreground text-xl font-bold tracking-tight">
+              Mart
+            </h6>
+          </Link>
+        </div>
+        {/* //INFO: BUTTONS & AUTH */}
+        <div className="flex flex-shrink-0 items-center gap-4 p-1">
+          <DarkMode />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderDashboard;
---
+++ src/modules/presentation/dashboard-page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex w-full flex-col justify-between pt-[4rem] pb-[0rem]">
+      <div className="flex-1 p-2">
+        <h2 className="text-xl font-semibold">Dashboard Content</h2>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
